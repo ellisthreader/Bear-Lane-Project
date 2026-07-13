9d24f97a1cf9d0bc477ba3cd7f97cccd6f87d51e
ellisthreader <ellis.threader3001@gmail.com>
2026-07-13 14:53:07 +0100

    Seed homepage product rails
    Co-authored-by: Ellis Threader <230532169+ellisthreader@users.noreply.github.com>

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
             AdminUserSeeder::class,
             CategorySeeder::class, // Must run first
             ProductSeeder::class,  // Depends on categories
+            HomepageProductSeeder::class,
         ]);
     }
 }
